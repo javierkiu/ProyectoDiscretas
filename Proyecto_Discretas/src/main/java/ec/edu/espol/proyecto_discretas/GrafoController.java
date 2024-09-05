2b60c080c6fb0a47e21674b662396bd50f2ef1d2
StevenMiraba <wsmiraba@espol.edu.ec>
2024-09-04 23:38:58 -0500

    GRAFO

--- Proyecto_Discretas/src/main/java/ec/edu/espol/proyecto_discretas/GrafoController.java
+++ Proyecto_Discretas/src/main/java/ec/edu/espol/proyecto_discretas/GrafoController.java
@@ -1,13 +1,23 @@
 package ec.edu.espol.proyecto_discretas;
 
+import Grafos.*;
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-
-import javafx.scene.input.MouseEvent;
-
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
 
 /**
  * FXML Controller class
@@ -15,7 +25,7 @@ import javafx.fxml.Initializable;
  * @author Steven Mirabá
  */
 public class GrafoController implements Initializable {
-
+    
     /**
      * Initializes the controller class.
      */
@@ -23,7 +33,7 @@ public class GrafoController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }
-   
+     
     @FXML
     private void volver(MouseEvent event) throws IOException {
         App.cambiarPantalla("primary", 1000, 600, event);
